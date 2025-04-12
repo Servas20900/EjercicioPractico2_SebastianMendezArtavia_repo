54fe21d5851d557782f9228a6a94433a2f7f1f49
Sebastián Roberto Méndez Artavia <sebasmendeza09@gmail.com>
2025-04-11 23:30:24 -0600

    UsusarioService
    UsusarioService

---
+++ CasoEstudio2/src/main/java/CasoEstudio2/Caso2/service/UsuarioService.java
@@ -0,0 +1,30 @@
+package CasoEstudio2.Caso2.service;
+
+
+import java.util.List;
+import CasoEstudio2.Caso2.domain.Usuario;
+
+public interface UsuarioService {
+    
+    // Se obtiene un listado de usuarios en un List
+    public List<Usuario> getUsuarios();
+    
+    // Se obtiene un Usuario, a partir del id de un usuario
+    public Usuario getUsuario(Usuario usuario);
+    
+    // Se obtiene un Usuario, a partir del username de un usuario
+    public Usuario getUsuarioPorUsername(String username);
+
+    // Se obtiene un Usuario, a partir del username y el password de un usuario
+    public Usuario getUsuarioPorUsernameYPassword(String username, String password);
+    
+    // Se obtiene un Usuario, a partir del username o el correo del usuario
+    public Usuario getUsuarioPorUsernameOCorreo(String username, String correo);
+    
+    // Se inserta un nuevo usuario si el id del usuario está vacío
+    // Se actualiza un usuario si el id del usuario NO está vacío
+    public void save(Usuario usuario, boolean crearRolUser);
+    
+    // Se elimina el usuario que tiene el id pasado por parámetro
+    public void delete(Usuario usuario);
+}
